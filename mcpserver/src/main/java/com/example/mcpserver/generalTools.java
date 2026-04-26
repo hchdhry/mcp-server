6b03a464ff39d130284f30387aa6395d775b0808
unknown <hchoudhury20@hotmail.com>
2026-04-26 17:34:22 +0100

    add tool to get current flights

--- mcpserver/src/main/java/com/example/mcpserver/generalTools.java
+++ mcpserver/src/main/java/com/example/mcpserver/generalTools.java
@@ -1,15 +1,34 @@
 package com.example.mcpserver;
 
-import com.ethlo.time.DateTime;
 import org.springframework.ai.mcp.annotation.McpTool;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.time.LocalDateTime;
 
 @Component
 public class generalTools {
+    @Value("${aviationstack.api-key}")
+    private String apiKey;
+
+    private WebClient client = WebClient.create("http://api.aviationstack.com/v1");
+
+
     @McpTool(name = "getCurrentTime", description = "return the current time")
-    public LocalDateTime getCurrentTime(){
+    public LocalDateTime getCurrentTime() {
         return LocalDateTime.now();
     }
+
+    @McpTool(name = "getFlights", description = "get flights using api")
+    public String getFlights() {
+        return client.get().uri(uriBuilder -> uriBuilder
+                .path("/flights")
+                .queryParam("access_key", apiKey)
+                .build()
+        ).retrieve().bodyToMono(String.class).block();
+
+
+    }
+
 }
